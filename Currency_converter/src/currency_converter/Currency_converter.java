9d61b23e89b1e3194f93bea819625d1b2c98b347
Hasebul Hasan <hasebulhasan44113@gmail.com>
2023-12-06 02:46:01 +0600

    Initial commit

---
+++ Currency_converter/src/currency_converter/Currency_converter.java
@@ -0,0 +1,21 @@
+package currency_converter;
+
+/**
+ *
+ * @author tuhan
+ */
+public class Currency_converter {
+   public static void converter(){
+       JFrame f = new JFrame("Converter");
+       JLabel 11,12;
+       JtextField t1, t2;
+       JButton b1,b2,b3; 
+       
+       11 = new Jabel("Tk");
+       11.setBound
+   }            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
